4d0d9fbb81078877de9190edfbcb4d2b80fbf77e
AVIDI AMRUTHAVALLI <avidiamruthavalli@gmail.com>
2025-03-27 12:42:14 +0530

    made changes in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  plugins: [react(),tailwind()],
+  plugins: [react(), tailwind()],
   base: "/",
   build: {
     outDir: "dist"
