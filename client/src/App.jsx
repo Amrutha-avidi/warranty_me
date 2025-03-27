3d03e878b0de3a970c502627038369db277d857e
AVIDI AMRUTHAVALLI <avidiamruthavalli@gmail.com>
2025-03-27 13:04:40 +0530

    made changes in App.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,28 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
-import Home from "./components/Home"; // Create a Home page component
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import Home from "./Home";
+import Login from "./Login";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/home" element={<IgnoreQueryParams component={Home} />} />
       </Routes>
     </Router>
   );
 }
+
+// Helper to remove query params (prevents 404)
+const IgnoreQueryParams = ({ component: Component }) => {
+  const location = useLocation();
+  const pathname = location.pathname; // Only keep "/home"
+  
+  if (pathname.startsWith("/home")) {
+    return <Component />;
+  }
+  
+  return null;
+};
 
 export default App;
